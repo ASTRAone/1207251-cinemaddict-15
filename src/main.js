934552f85d7106d8c0ba8e71934999763460a843
Vladislav <vladway827@gmail.com>
2021-07-27 21:58:06 +0300

    added views

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { renderShowMoreButton } from './view/show-more-button';
+import { createFilterCinema } from './view/filter';
+import { renderRatingUser } from './view/rating-user';
+import { renderCardFilm } from './view/card-film';
+import { renderFilmDetails } from './view/film-details';
---
+++ src/view/show-more-button.js
@@ -0,0 +1,1 @@
+export const renderShowMoreButton = () => '<button class="films-list__show-more">Show more</button>';
