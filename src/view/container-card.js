642a0b1ecf5d7fcce87bdb7bcf3ebc82a9ecb97d
Vladislav <vladway827@gmail.com>
2021-08-07 12:34:52 +0300

    fix modules

--- src/view/container-card.js
+++ src/view/container-card.js
@@ -1,14 +1,15 @@
-const renderMoviesCard = ({ comments, filmInfo }) =>
-  `<article class="film-card">
-        <h3 class="film-card__title">${filmInfo.title}</h3>
-        <p class="film-card__rating">${filmInfo.totalRating}</p>
+const renderMoviesCard = ({ comments, filmInfo }) => {
+  const { title, totalRating, release, runtime, genre, poster, description } = filmInfo;
+  return `<article class="film-card">
+        <h3 class="film-card__title">${title}</h3>
+        <p class="film-card__rating">${totalRating}</p>
         <p class="film-card__info">
-            <span class="film-card__year">${filmInfo.release.date}</span>
-            <span class="film-card__duration">${filmInfo.runtime}</span>
-            <span class="film-card__genre">${filmInfo.genre}</span>
+            <span class="film-card__year">${release.date}</span>
+            <span class="film-card__duration">${runtime}</span>
+            <span class="film-card__genre">${genre}</span>
         </p>
-        <img src=${filmInfo.poster} alt="" class="film-card__poster">
-        <p class="film-card__description">${filmInfo.description}</p>
+        <img src=${poster} alt="" class="film-card__poster">
+        <p class="film-card__description">${description}</p>
         <a class="film-card__comments">${comments.length} comments</a>
         <div class="film-card__controls">
             <button class="film-card__controls-item film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
@@ -16,6 +17,7 @@ const renderMoviesCard = ({ comments, filmInfo }) =>
             <button class="film-card__controls-item film-card__controls-item--favorite" type="button">Mark as favorite</button>
         </div>
     </article>`;
+};
 
 const renderCards = (cards = []) =>
   cards.map((item) => renderMoviesCard({ ...item })).join('');
